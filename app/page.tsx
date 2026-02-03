7ee5dbce394c2574deb7b48d4bbcda2fa3873785
Giuliana Guidi <89464824+giulianaguidi@users.noreply.github.com>
2026-02-02 21:22:56 -0300

    new component for description

--- app/page.tsx
+++ app/page.tsx
@@ -17,9 +17,9 @@ export default function Home() {
         <Image
           src="/AGLogo.png"
           alt="Logo AG"
-          width={120}
-          height={120}
-          className="mb-4"
+          width={150}
+          height={150}
+          className="mb-4 invert"
         />
         <h1 className="text-2xl font-bold">
           Estudio Jurídico Adrián Gustavo Guidi
@@ -36,17 +36,22 @@ export default function Home() {
       >
         <h2 className="text-xl font-semibold text-primary mb-4">Bienvenidos</h2>
         <p className="text-base leading-relaxed">
-          Es un honor darle la bienvenida a mi despacho legal. Soy Adrián Gustavo Guidi,
-          abogado comprometido con la asesoría legal de calidad y el servicio a la comunidad.
+          Es un honor darle la bienvenida a mi despacho legal. Soy Adrián
+          Gustavo Guidi, abogado comprometido con la asesoría legal de calidad y
+          el servicio a la comunidad.
         </p>
         <p className="text-base leading-relaxed mt-4">
-          Mi compromiso es ofrecer un servicio legal comprensivo, ético y profesional.
+          Mi compromiso es ofrecer un servicio legal comprensivo, ético y
+          profesional.
         </p>
         <p className="text-base leading-relaxed mt-4">
-          Gracias por considerar mi despacho para sus necesidades legales. Espero poder brindarle la ayuda que necesita.
+          Gracias por considerar mi despacho para sus necesidades legales.
+          Espero poder brindarle la ayuda que necesita.
         </p>
         <p className="mt-6 font-semibold text-accent">
-          Atentamente,<br />Adrián Gustavo Guidi
+          Atentamente,
+          <br />
+          Adrián Gustavo Guidi
         </p>
       </motion.section>
 
@@ -63,36 +68,100 @@ export default function Home() {
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 max-w-4xl mx-auto">
           {[
-            "Derecho Civil y Comercial",
-            "Sucesiones",
-            "Accidentes de tránsito",
-            "Daños y Perjuicios",
-            "Juicios Ejecutivos",
-            "Desalojos",
-            "Contratos",
-            "Divorcios y uniones convivenciales",
-            "Cuota alimentaria",
-            "Impedimento de contacto",
-            "Régimen de comunicación",
-            "Cuidado personal",
-            "Filiación",
-            "Adopción",
-            "Responsabilidad parental",
-            "Accidentes de trabajo",
-            "Enfermedades Profesionales",
-            "Indemnizaciones por despido",
-            "Reclamos laborales en general",
-            "Causas Federales",
-          ].map((servicio, index) => (
+            {
+              nombre: "Derecho Civil y Comercial",
+              descripcion: "",
+            },
+            {
+              nombre: "Sucesiones",
+              descripcion: "",
+            },
+            { nombre: "Accidentes de tránsito", descripcion: "" },
+            {
+              nombre: "Infracciones de tránsito",
+              descripcion: "Restitución de licencia",
+            },
+            {
+              nombre: "Daños y Perjuicios",
+              descripcion: "",
+            },
+            {
+              nombre: "Juicios Ejecutivos",
+              descripcion: "",
+            },
+            {
+              nombre: "Desalojos",
+              descripcion: "",
+            },
+            {
+              nombre: "Contratos",
+              descripcion: "",
+            },
+            {
+              nombre: "Divorcios y uniones convivenciales",
+              descripcion: "",
+            },
+            {
+              nombre: "Cuota alimentaria",
+              descripcion: "",
+            },
+            {
+              nombre: "Impedimento de contacto",
+              descripcion: "",
+            },
+            {
+              nombre: "Régimen de comunicación",
+              descripcion: "",
+            },
+            {
+              nombre: "Cuidado personal",
+              descripcion: "",
+            },
+            {
+              nombre: "Filiación",
+              descripcion: "",
+            },
+            {
+              nombre: "Adopción",
+              descripcion: "",
+            },
+            {
+              nombre: "Responsabilidad parental",
+              descripcion: "",
+            },
+            {
+              nombre: "Accidentes de trabajo",
+              descripcion: "",
+            },
+            {
+              nombre: "Enfermedades Profesionales",
+              descripcion: "",
+            },
+            {
+              nombre: "Indemnizaciones por despido",
+              descripcion: "",
+            },
+            {
+              nombre: "Reclamos laborales en general",
+              descripcion: "",
+            },
+            {
+              nombre: "Causas Federales",
+              descripcion: "",
+            },
+          ].map((servicio) => (
             <motion.div
-              key={index}
+              key={servicio.nombre}
               whileHover={{ scale: 1.03 }}
               transition={{ duration: 0.3 }}
             >
-              <Card className="shadow-md hover:shadow-lg transition-shadow duration-300">
-                <CardContent className="p-4 text-center font-medium text-text">
-                  {servicio}
-                </CardContent>
+              <Card className="shadow-md hover:shadow-lg transition-shadow duration-300 gap-1 p-3 h-[10dvh]">
+                <div className="text-center font-medium text-text">
+                  {servicio.nombre}
+                </div>
+                <div className="text-center font-medium text-black/50 text-sm">
+                  {servicio.descripcion}
+                </div>
               </Card>
             </motion.div>
           ))}
@@ -110,7 +179,9 @@ export default function Home() {
         <h2 className="text-xl font-semibold text-primary mb-6">Contacto</h2>
         <div className="space-y-4 max-w-md mx-auto">
           <div>
-            <p className="text-sm font-semibold uppercase text-text">Dirección</p>
+            <p className="text-sm font-semibold uppercase text-text">
+              Dirección
+            </p>
             <p>Bahía Blanca, Alsina 35, Piso 7, Oficina 4</p>
           </div>
           <div>
@@ -118,7 +189,9 @@ export default function Home() {
             <p>estudioadrianguidi@gmail.com</p>
           </div>
           <div>
-            <p className="text-sm font-semibold uppercase text-text">Días y Horarios</p>
+            <p className="text-sm font-semibold uppercase text-text">
+              Días y Horarios
+            </p>
             <p>Lunes a Jueves de 17 a 20hs</p>
           </div>
         </div>
